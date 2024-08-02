b5e3edc22a846a4d5aed1ddfc449d1f78ea57ca6
Chris Maltby <chris.maltby@gmail.com>
2024-08-02 15:05:51 +0100

    Remove unused legacyTypes

--- src/shared/lib/entities/legacyTypes.ts
+++
@@ -1,6 +0,0 @@
-import { Actor, Scene, Trigger } from "shared/lib/entities/entitiesTypes";
-
-export type LegacyScene = Omit<Scene, "actors" | "triggers"> & {
-  actors: Actor[];
-  triggers: Trigger[];
-};
